95515c265637c32b44cb0a778edabc98586a943c
Ytfarc <crafty@ua.fm>
2015-07-11 14:34:10 +0300

    Updated Changelog.ru

--- Source/version.h
+++ Source/version.h
@@ -3,8 +3,8 @@
 #define TARGETVERSION "Fallout 1 v1.2 US"
 
 #define VERSION_MAJOR 1
-#define VERSION_MINOR 2
+#define VERSION_MINOR 3
 #define VERSION_BUILD 0
 #define VERSION_REV 0
 
-#define VERSION_STRING "v1.2"
+#define VERSION_STRING "v1.3"
